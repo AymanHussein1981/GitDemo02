0c1c40a797950f256fa637e239de3c9a03b4b94e
AymanHussein1981 <ayman42@hotmail.com>
2022-01-07 00:28:25 -0600

    Third Time

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -8,8 +8,12 @@ public class RestAPITest {
 	public void APIApp()
 	{
 		System.out.println("APIApp");
-		System.out.println("APIApp01");
-		System.out.println("APIApp02");
+		System.out.println("APIApp1");
+		System.out.println("APIApp2");
+		System.out.println("APIApp3");
+		System.out.println("APIApp4");
+		System.out.println("APIApp5");
+		
 		
 	}
 	
